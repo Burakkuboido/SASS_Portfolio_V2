ac1cc710dd96f951b1fe875d73a95829556e0139
Burakkuboido <Burakkuboido@hotmail.com>
2023-08-17 19:48:21 +0000

    Home page name animation implemented

---
+++ dist/js/main.js
@@ -0,0 +1,21 @@
+const homeMenu = document.querySelector('.home');
+const arrow = document.querySelector('.arrow');
+
+let showHome = false;
+
+arrow.addEventListener('click', toggleHome);
+
+function toggleHome() {
+    if(!showHome) {
+        homeMenu.classList.add('open');
+        arrow.classList.add('open_visible');
+
+        showHome = true;
+    } else {
+        homeMenu.classList.remove('open');
+        arrow.classList.remove('open_visible');
+
+        showHome = false;
+    }
+
+}
